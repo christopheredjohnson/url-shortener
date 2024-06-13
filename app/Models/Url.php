4180ac8be087f05ca22c706b6e35a3a2235435e5
Christophere Johnson <christophere.johnson@GS-DEV1.local>
2024-06-13 12:45:50 -0400

    handle unique short_urls

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -16,7 +16,19 @@ class Url extends Model
     protected static function booted(): void {
         static::creating(function(Url $url) {
 
-            $url->short_url = Str::random(30);
+            $url->short_url = Url::generateUniqueShortUrl(30);
         });
+    }
+
+    static function generateUniqueShortUrl($length = 10)
+    {
+        $randomString = Str::random($length);
+
+        // Check if the generated string already exists in the database
+        while (Url::where('short_url', $randomString)->exists()) {
+            $randomString = Str::random($length); // Regenerate random string if it exists
+        }
+
+        return $randomString;
     }
 }
